4e8b4e423d7379852fb40b23f73833d4b5d79ea6
Hemant Kumar Singh <hemant@Hemants-MacBook-Air.local>
2020-05-25 18:12:09 +0530

    Updates

---
+++ precache-manifest.57478cb415b299f16a986a1f3f3ebbb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a364761481aa23f2000c8f90b6f6ab5a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "f5752222345d0541e5d9",
+    "url": "/robofriends/static/css/main.d6e0016a.chunk.css"
+  },
+  {
+    "revision": "e6274bb51b25f098d5e8",
+    "url": "/robofriends/static/js/2.0d983b4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.0d983b4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5752222345d0541e5d9",
+    "url": "/robofriends/static/js/main.0237a469.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.31a440cb7721f0f463bf99f197f434da.js"
+  "/robofriends/precache-manifest.57478cb415b299f16a986a1f3f3ebbb2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robofriends/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
