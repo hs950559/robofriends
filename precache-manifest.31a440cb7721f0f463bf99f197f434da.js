14015a3b26d7ade78abcd78b5cd96ec0f07e9d2d
Hemant Kumar Singh <hemant@Hemants-MacBook-Air.local>
2020-05-25 18:04:14 +0530

    Updates

---
+++ precache-manifest.31a440cb7721f0f463bf99f197f434da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9fe8c257aabc5948d8a81b3d1da33ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04e3f1f88a3ee1af91a4",
+    "url": "/static/css/main.1b091aa9.chunk.css"
+  },
+  {
+    "revision": "e6274bb51b25f098d5e8",
+    "url": "/static/js/2.0d983b4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0d983b4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04e3f1f88a3ee1af91a4",
+    "url": "/static/js/main.9b7166f1.chunk.js"
+  },
+  {
+    "revision": "fdb66bf0534857f19626",
+    "url": "/static/js/runtime-main.8f3ae637.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
